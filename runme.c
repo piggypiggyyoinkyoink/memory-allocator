867aa7df76f5b38f39a8f305834f84fa20d18613
piggypiggyyoinkyoink <quackquack904@gmail.com>
2025-11-22 21:11:18 +0000

    cleaned up runme.c

--- runme.c
+++ runme.c
@@ -23,8 +23,13 @@ int main(){
     // run init
     mm_init(b, 800);
 
+    printf("\n\nAllocating ptr");
     void* ptr = mm_malloc(70);
+
+    printf("\n\nAllocating ptr2");
     void* ptr2 = mm_malloc(70);
+
+    printf("\n\nAllocating ptr3");
     void* ptr3 = mm_malloc(700);
 
     printf("\n\nFreeing ptr");
@@ -32,7 +37,20 @@ int main(){
 
     printf("\n\nFreeing ptr2");
     mm_free(ptr2);
+
+    printf("\n\nAllocating ptr4");
     void* ptr4 = mm_malloc(100);
+    
+    printf("\n\nAllocating ptr5");
+    void* ptr5 = mm_malloc(120);
+
+    printf("\n\nAllocating ptr6");
+    void* ptr6 = mm_malloc(100);
+    printf("\n\nFreeing ptr5");
+    mm_free(ptr5);
+
+    printf("\n\nFreeing ptr6");
+    mm_free(ptr6);
     void* buf[256];
     int arr[100] = {6};
     void* src = arr;
@@ -42,9 +60,11 @@ int main(){
     int x = mm_read(ptr4, 0, buf, 256);
     printf("\n READ DATA: %d", x);
 
-    printf("\nFreeing ptr4");
+    printf("\n\nFreeing ptr4");
     mm_free(ptr4);
+    printf("\n\nFreeing ptr4 (double-free test)");
 
+    mm_free(ptr4);
     
     free(b);
     return 0;
