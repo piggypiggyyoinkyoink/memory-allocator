fb7937cb543410c269d9f8c26f79083b15df5f18
piggypiggyyoinkyoink <quackquack904@gmail.com>
2025-11-23 14:35:34 +0000

    mm_malloc now returns data pointer instead of node pointer, other funcs updated to handle this (IT WORKS ON THE AUTOGRADER OMG)

--- allocator.c
+++ allocator.c
@@ -155,7 +155,8 @@ void *mm_malloc(size_t size){
             found = 1;
             *currentNodePtr = currentNode;
             printf("\nALLOCATION SUCCESSFUL");
-            return currentNodePtr;
+            void* dataPtr = currentNode.data;
+            return dataPtr;
         }else if (currentNode.next == NULL){
             //no available nodes of sufficient size
             end = 1;
@@ -178,6 +179,7 @@ void *mm_malloc(size_t size){
 // Safely read data from an allocated block starting at offset bytes into buf.
 // Returns the number of bytes read, or -1 if corruption or invalid pointer detected.
 int mm_read(void *ptr, size_t offset, void *buf, size_t len){
+    ptr = (void*)((uint8_t*)ptr - sizeof(struct Node));
     //return if pointer invalid
     if (((uint8_t*)ptr) < heapPtr or ((uint8_t*)ptr) > (heapPtr + heapSize)){
         printf("\nINVALID POINTER IN MM_READ");
@@ -219,6 +221,8 @@ int mm_read(void *ptr, size_t offset, void *buf, size_t len){
 // Safely write data into an allocated block starting at offset bytes from src.
 // Returns the number of bytes written, or -1 if corruption or invalid pointer detected.
 int mm_write(void *ptr, size_t offset, const void *src, size_t len){
+    ptr = (void*)((uint8_t*)ptr - sizeof(struct Node));
+
     //return if pointer invalid
     if (((uint8_t*)ptr) < heapPtr or ((uint8_t*)ptr) > (heapPtr + heapSize)){
         printf("\nINVALID POINTER IN MM_WRITE");
@@ -367,6 +371,8 @@ void merge_node(struct Node* nodePtr){
 // Free a previously-allocated pointer (ignore NULL).
 // Must detect double-free.
 void mm_free(void *ptr){
+    ptr = (void*)((uint8_t*)ptr - sizeof(struct Node));
+
     //check for null pointer
     if (ptr == NULL){
         //fuck off if null
